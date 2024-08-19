eb110f71570832b02621a388d218efe24938cff6
Tej Pratap <tejp@techfoursolutions.com>
2024-08-19 16:53:31 +0530

    commit added

---
+++ 02_basic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+a=10
+name="Tej Pratap"
+age=24
+
+echo "My Name is $name and age is $age";
+
+git status
+git add .
+git commit -m "commit added"
+git push
---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#this is single line comment
+
+<<abc
+
+this
+is
+multi line comment whatever you write after
+<<here you have to close
+by writing that same word
+at the end
+abc
+
+echo "script started"
+echo $0
+#ssh ubuntu@142.44.247.133
+
